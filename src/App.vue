7c1995978358729b5bba56ba0b38e48dc5f3c4ea
TrungNQ <trung@saisystem.vn>
2020-04-17 16:40:09 +0700

    taoj webpack vaf cacs component trong vue. xaay dựng chức năng new game.

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+	<div id="app">
+            <div class="wrapper clearfix">
+            <players 
+                v-bind:scorePlayer="scorePlayer" 
+                v-bind:activePlayer="activePlayer" 
+                v-bind:currentScore="currentScore" 
+            />
+            <controls 
+              v-on:handleNewGame="handleNewGame"
+            />
+            <dices
+               v-bind:dices="dices" 
+            />
+            <popup-rule
+              v-bind:isOpenPopup="isOpenPopup"
+              v-on:handleConfirm="handleConfirm"
+            />
+        </div>
+	</div>
+</template>
+
+<script>
+
+import  Players from   './components/Players'
+import  Controls from  './components/Controls'
+import Dices from './components/Dices'
+import PopupRule from './components/PopupRule'
+export default {
+	name: 'app',
+	data () {
+		return {
+            isPlaying:false,
+            scorePlayer:[13,30],
+            activePlayer:1,
+            currentScore : 0,
+            isOpenPopup:false,
+            dices:[5,5]
+		}
+	},
+	components: {
+      Players,
+      Controls,
+      Dices,
+      PopupRule
+    },
+    computed: {
+        
+    },
+    methods: {  
+        handleNewGame(){
+            this.isOpenPopup = true;
+        },
+
+        handleConfirm(){
+            this.isPlaying = true;
+            this.isOpenPopup = false;
+            this.activePlayer = 0;
+            this.scorePlayer = [0,0];
+            this.currentScore = 0;
+
+        }
+    }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    
+}
+
+.clearfix::after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+body {
+    background-image: linear-gradient(rgba(62, 20, 20, 0.4), rgba(62, 20, 20, 0.4)), url('/public/assets/back.jpg');
+    background-size: cover;
+    background-position: center;
+    font-family: Lato;
+    font-weight: 300;
+    position: relative;
+    height: 100vh;
+    color: #555;
+}
+
+.wrapper {
+    width: 1000px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.3);
+    overflow: hidden;
+}
+</style>
---
+++ src/components/PopupRule.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="wrapper-popup " v-bind:class="getClassPopup">
+        <div class="rule">
+            <h3>Luat choi</h3>
+            <p>Luat choiLuat choiLuat choiLuat choiLuat choiLuat choi</p>
+            <button class="confirm" v-on:click="confirm">Da hieu </button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"popup-rule",
+    props:{
+        isOpenPopup : {type:Boolean,default:false}
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        confirm(){
+            this.$emit('handleConfirm');
+        }
+    },
+    computed: {
+        getClassPopup(){
+            return { 'open-popup':this.isOpenPopup }
+        }
+    },
+}
+</script>
+<style lang="">
+    .wrapper-popup {
+        position:fixed;
+        top : 0;
+        left:0;
+        bottom:0;
+        right:0;
+        background-color:rgba(0,0,0,0.4);
+        opacity: 0;
+        visibility:hidden;
+         transition:all .3s ease
+    }
+
+    .wrapper-popup.open-popup {
+        opacity: 1;
+        visibility:visible
+    }
+
+    .rule{
+        position:absolute;
+        width:300px;
+        padding:20px;
+        top:40%;
+        left:40%;
+        background-color:#fff;
+        transition:translate(-50%,-50%) scale(1.2);
+        font-family:Arial;
+         transition:all .3s ease
+    }
+    .open-popup .rule {
+
+        transition:translate(-50%,-50%) scale(1);
+    }
+    .rule .confirm{
+        margin-top:20px;
+        padding:8px 15px;
+        border:2px solid #333;
+        background-color:#fff;
+        transition:all .3s ease
+    }
+    .rule .confirm:hover{
+       color:#fff;
+        background-color:#333
+    }
+</style>
